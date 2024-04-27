264714e0fc494d5d31e3573375696a72c4ca3a80
abdelrahman gamal <abdelrahmanbetter@stud.cu.edu.eg>
2024-04-27 17:23:40 +0300

    create project of the count the clendar of the student

---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+       Scanner input = new Scanner(System.in);
+
+        int  boy=0;
+        int  girl=0;
+        System.out.println("السلام عليكم ورحمه الله وبراكاته ");
+        System.out.println("ازيكم يا طلاب المعهد  ");
+        System.out.println("انا اخوكم عبدالله اريد عمل تطبيق لمساعدتكم في التقدم   ");
+        System.out.println("سأقوم بسؤال كل واحد منكم سؤال لكي انفذ فكرة المشروع ");
+        System.out.println(" برجاء تقبل مني ذلك  !!! ");
+        for(int i=0; i<10;i++){
+            System.out.println( " ما هو نوعك ؟؟");
+
+            if(input.next().equals("ولد"))
+          {
+            boy++ ;
+          }
+         else {
+              girl++;
+          }
+
+          }
+        System.out.println(" يا ادارة المعهد الكرام (يا كتله) ");
+        System.out.println(" اود ان اعلم سيادتكم ان المعهد به عدد الطلاب كالاتي . ");
+        System.out.print("عدد البنين  ");
+        System.out.print(boy);
+        System.out.print("عدد البنات   ");
+        System.out.print(girl);
+
+        }
+
+    }
